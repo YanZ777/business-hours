e5e94ec8abda3dd0ed04578fd3a9aac05e72c304
Yan <yanzhao777@gmail.com>
2024-04-10 19:40:36 -0700

    test: fix tests since the verbage got changed

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import App from 'business-hours/App';
 
 test('renders App', () => {
-   const text = 'Hello World!';
+   const text = 'Business Hours';
    render(<App />);
    expect(screen.getByText(text)).toBeInTheDocument();
 });

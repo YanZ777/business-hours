497db39e48634167bee7b0bdce401f8101e9703e
Yan <yanzhao777@gmail.com>
2024-04-25 17:46:20 -0700

    fix: fix broken eslint config

--- web/src/YelpInterface.js
+++ web/src/YelpInterface.js
@@ -1,7 +1,9 @@
 const searchUrl = 'https://api.yelp.com/v3/autocomplete';
 
-function yelpSearch(searchTerm) {
+function yelpSearch() {
+   /*
    console.log(`You searched for ${searchTerm}!`);
+   */
 }
 
 export { searchUrl, yelpSearch };
